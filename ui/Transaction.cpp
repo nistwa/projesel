a7d52b45ea0f775850e809b571d469d0ab6f57b3
azratopal75-gif <azra.topal.75@gmail.com>
2026-05-17 20:58:25 +0300

    Add files via upload

---
+++ ui/Transaction.cpp
@@ -0,0 +1,6 @@
+#include "Transaction.h"
+
+// Transaction soyut sinif oldugu icin burada ekstra
+// implementasyon gerekmez. Constructor ve destructor
+// header'da inline tanimli.
+// Bu dosya projeye dahil edilmeli ki linker hata vermsin.
