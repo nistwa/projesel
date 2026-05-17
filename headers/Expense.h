29167de0bcf62afb7c17193456e10a00222a8057
Nisanur Özpehlivan <126330681+nistwa@users.noreply.github.com>
2026-05-17 17:57:31 +0300

    Update Expense.h

--- headers/Expense.h
+++ headers/Expense.h
@@ -1,1 +1,7 @@
+#include "Transaction.h"
 
+class Expense : public Transaction {
+public:
+    Expense(double amt, QString cat, QDate dt) : Transaction(amt, cat, dt) {}
+    QString getType() const override { return "Expense"; }
+};
