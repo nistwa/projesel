b858e3ac8d9ae11ef9a1fa7022ba4de08d709de0
azratopal75-gif <azra.topal.75@gmail.com>
2026-05-17 21:15:15 +0300

    Delete Transaction.cpp

--- Transaction.cpp
+++
@@ -1,6 +0,0 @@
-#include "Transaction.h"
-
-// Transaction soyut sinif oldugu icin burada ekstra
-// implementasyon gerekmez. Constructor ve destructor
-// header'da inline tanimli.
-// Bu dosya projeye dahil edilmeli ki linker hata vermsin.
