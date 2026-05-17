0fcd06918881263e5897979339eb7c416b4dba7b
azratopal75-gif <azra.topal.75@gmail.com>
2026-05-17 20:50:25 +0300

    Update Transaction.cpp

--- src/Transaction.cpp
+++ src/Transaction.cpp
@@ -1,1 +1,7 @@
 
+#include "Transaction.h"
+
+// Transaction soyut sinif oldugu icin burada ekstra
+// implementasyon gerekmez. Constructor ve destructor
+// header'da inline tanimli.
+// Bu dosya projeye dahil edilmeli ki linker hata vermsin.
