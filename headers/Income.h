0a2d0b0eec2e0d7da18f635b4694b0416fc8b6cb
Nisanur Özpehlivan <126330681+nistwa@users.noreply.github.com>
2026-05-17 17:57:10 +0300

    Update Income.h

--- headers/Income.h
+++ headers/Income.h
@@ -1,1 +1,7 @@
+#include "Transaction.h"
 
+class Income : public Transaction {
+public:
+    Income(double amt, QString cat, QDate dt) : Transaction(amt, cat, dt) {}
+    QString getType() const override { return "Income"; }
+};
