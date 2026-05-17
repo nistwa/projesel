d53546485f3e10452307af5ce5025bb2d2c5eccd
azratopal75-gif <azra.topal.75@gmail.com>
2026-05-17 21:14:49 +0300

    Delete Expense.cpp

--- Expense.cpp
+++
@@ -1,5 +0,0 @@
-#include "Expense.h"
-
-// Expense sinifi Transaction'dan turetilmistir (Kalitim).
-// getType() fonksiyonu header'da override edildi.
-// Ek bir is mantigi gerekmediginden .cpp minimal kalir.
