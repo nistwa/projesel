83e6a67af3cacacd262529c18a69f158a0d04b89
azratopal75-gif <azra.topal.75@gmail.com>
2026-05-17 21:15:02 +0300

    Delete Income.cpp

--- Income.cpp
+++
@@ -1,5 +0,0 @@
-#include "Income.h"
-
-// Income sinifi Transaction'dan turetilmistir (Kalitim).
-// getType() fonksiyonu header'da override edildi.
-// Ek bir is mantigi gerekmediginden .cpp minimal kalir.
